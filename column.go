8030eb97a8738a48e595f4be7a973a155711a276
natan <mrnatande@gmail.com>
2017-08-20 11:29:25 +0800

    check type

--- column.go
+++ column.go
@@ -175,6 +175,10 @@ func parseColumnInfo(typ reflect.Type) *columnInfo {
 }
 
 func isSupportType(typ reflect.Type) bool {
+	if typ == nil {
+		return false
+	}
+
 	switch typ.Kind() {
 	case reflect.Bool, reflect.Float32, reflect.Float64, reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
 		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.String:
