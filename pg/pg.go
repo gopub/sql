f7d14f8663946caf108c9134b1ae647aac73522d
nat <mrnatande@gmail.com>
2020-12-11 10:17:22 -0500

    Try tcp socket and unix socket

--- pg/pg.go
+++ pg/pg.go
@@ -15,35 +15,73 @@ func BuildURL(name, host string, port int, user, password string, sslEnabled boo
 	if port == 0 {
 		port = 5432
 	}
-	url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", user, password, host, port, name)
+	url := fmt.Sprintf("%s:%d/%s", host, port, name)
+	if user == "" {
+		url = "postgres://" + url
+	} else {
+		if password == "" {
+			url = "postgres://" + user + "@" + url
+		} else {
+			url = "postgres://" + user + ":" + password + "@" + url
+		}
+	}
 	if !sslEnabled {
 		url = url + "?sslmode=disable"
 	}
 	return url
 }
 
-func Open(dbURL string) *sql.DB {
+func Open(dbURL string) (*sql.DB, error) {
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		log.Panicf("Open postgres %s: %+v", dbURL, err)
+		return nil, fmt.Errorf("open: %w", err)
 	}
 
 	err = db.Ping()
 	if err != nil {
-		log.Panicf("Ping %s: %+v", dbURL, err)
+		return nil, fmt.Errorf("ping: %w", err)
+	}
+	return db, nil
+}
+
+func MustOpen(dbURL string) *sql.DB {
+	db, err := Open(dbURL)
+	if err != nil {
+		log.Panicf("Cannot open %s: %v", dbURL, err)
 	}
 	return db
 }
 
-func LocalConnURL() string {
+func LocalConnURL(unixSocket bool) string {
 	u, err := user.Current()
 	if err != nil {
 		log.Errorf("Get current user: %v", err)
 		return ""
 	}
-	return fmt.Sprintf("postgres:///%s?host=/var/run/postgresql/", u.Username)
+	if unixSocket {
+		return fmt.Sprintf("postgres:///%s?host=/var/run/postgresql/", u.Username)
+	}
+	return BuildURL(u.Username, "localhost", 5432, u.Username, "", false)
 }
 
-func OpenLocalDB() *sql.DB {
-	return Open(LocalConnURL())
+func OpenLocalDB() (*sql.DB, error) {
+	url := LocalConnURL(false)
+	if db, err := Open(LocalConnURL(false)); err == nil {
+		log.Debugf("Connected to %s", url)
+		return db, nil
+	}
+	url = LocalConnURL(true)
+	db, err := Open(url)
+	if err == nil {
+		log.Debugf("Connected to %s", url)
+	}
+	return db, err
+}
+
+func MustOpenLocalDB() *sql.DB {
+	db, err := OpenLocalDB()
+	if err != nil {
+		log.Panicf("Cannot open local db: %v", err)
+	}
+	return db
 }
